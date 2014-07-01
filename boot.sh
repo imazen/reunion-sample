f707cae225c76157a0e469ea47ad123ba922fe10
Nathanael Jones <nathanael.jones@gmail.com>
2014-07-01 12:15:00 +0200

    Add port number, don't reboot when .bin changes.

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
-`bundle exec rerun --dir '..' --pattern '**/*.{rb,bin,slim,js,yml,css,less, scss,sass,erb,html,htmf,haml,ru,haml,md,mdown,markdown}' -- thin start --debug`
+`bundle exec rerun --dir '..' --pattern '**/*.{rb,slim,js,yml,css,less, scss,sass,erb,html,htmf,haml,ru,haml,md,mdown,markdown}' -- thin start --debug --port=3001`
